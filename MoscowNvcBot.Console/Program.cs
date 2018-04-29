7e314d606197fae7cbc894a969ccd25c134dfe7b
Vadim Borovikov <vadim.borovikov@gmail.com>
2018-04-30 02:56:02 +0300

    My first proper bot! (w/o token)

---
+++ MoscowNvcBot.Console/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Threading;
+using GoogleDocumentsUnifier.Logic;
+using Telegram.Bot.Types;
+
+namespace MoscowNvcBot.Console
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            string clientSecretPath = ConfigurationManager.AppSettings.Get("clientSecretPath");
+
+            string token = ConfigurationManager.AppSettings.Get("token");
+            string checklistId = ConfigurationManager.AppSettings.Get("checklistId");
+            string casesId = ConfigurationManager.AppSettings.Get("casesId");
+            string empathyId = ConfigurationManager.AppSettings.Get("empathyId");
+
+            using (var stream = new FileStream(clientSecretPath, FileMode.Open, FileAccess.Read))
+            {
+                string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                string credentialPath =
+                    Path.Combine(folderPath, ".credentials/drive-dotnet-quickstart.json");
+
+                using (var googleProvider =
+                    new GoogleApisDriveProvider(stream, credentialPath, "user", CancellationToken.None))
+                {
+                    var botLogic =
+                        new MoscowNvcBotLogc(token, checklistId, casesId, empathyId, googleProvider);
+
+                    User me = botLogic.Bot.GetMeAsync().Result;
+                    System.Console.Title = me.Username;
+
+                    botLogic.Bot.StartReceiving();
+                    System.Console.WriteLine($"Start listening for @{me.Username}");
+                    System.Console.ReadLine();
+                    botLogic.Bot.StopReceiving();
+                }
+            }
+        }
+    }
+}
+    
