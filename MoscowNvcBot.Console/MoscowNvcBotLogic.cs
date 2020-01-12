d37d19fe80bc4c8bab7ec22f3e097505b6db02d9
Vadim Borovikov <vadim.borovikov@gmail.com>
2020-01-12 16:46:46 +0300

    ChatAction now sends periodically until job is done

--- MoscowNvcBot.Console/MoscowNvcBotLogic.cs
+++ MoscowNvcBot.Console/MoscowNvcBotLogic.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using GoogleDocumentsUnifier.Logic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.InputFiles;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -19,6 +22,8 @@ namespace MoscowNvcBot.Console
         private readonly DataManager _googleDataManager;
 
         private readonly List<DocumentInfo> _infos;
+
+        private static readonly TimeSpan ChatActionDuration = TimeSpan.FromSeconds(5);
 
         public MoscowNvcBotLogic(string token, List<DocumentInfo> infos, DataManager googleDataManager)
         {
@@ -45,7 +50,7 @@ namespace MoscowNvcBot.Console
                     await ShowOptionsAsync(e.Message.Chat.Id);
                     break;
                 case "Все раздатки вместе":
-                    await SendGooglePdfAsync(e.Message.Chat.Id, "Все раздатки вместе.pdf", _infos);
+                    await SendAllGooglePdfAsync(e.Message.Chat.Id, "Все раздатки вместе.pdf");
                     break;
                 case "Все раздатки по отдельности":
                     foreach (DocumentInfo info in _infos)
@@ -54,50 +59,6 @@ namespace MoscowNvcBot.Console
                     }
                     break;
             }
-        }
-
-        private async Task SendGooglePdfAsync(long chatId, string fileName, IEnumerable<DocumentInfo> infos)
-        {
-            await Bot.SendChatActionAsync(chatId, ChatAction.UploadDocument);
-
-            IEnumerable<DocumentRequest> requests = infos.Select(info => new DocumentRequest(info, 1));
-
-            string path = Path.GetTempFileName();
-            _googleDataManager.Unify(requests, path, false);
-
-            await SendFileAsync(chatId, fileName, path);
-        }
-
-        private async Task SendGooglePdfAsync(long chatId, DocumentInfo info)
-        {
-            await Bot.SendChatActionAsync(chatId, ChatAction.UploadDocument);
-
-            var request = new DocumentRequest(info, 1);
-
-            string path = Path.GetTempFileName();
-            _googleDataManager.Copy(request, path, false);
-
-            string fileName = GetName(info);
-
-            await SendFileAsync(chatId, fileName, path);
-        }
-
-        private string GetName(DocumentInfo info)
-        {
-            string name = _googleDataManager.GetName(info.Id);
-            name = name.Replace("«", "");
-            name = name.Replace("»", "");
-            return $"{name}.pdf";
-        }
-
-        private async Task SendFileAsync(long chatId, string fileName, string path)
-        {
-            using (var fileStream = new FileStream(path, FileMode.Open))
-            {
-                var pdf = new InputOnlineFile(fileStream, fileName);
-                await Bot.SendDocumentAsync(chatId, pdf);
-            }
-            File.Delete(path);
         }
 
         private async Task ShowOptionsAsync(long chatId)
@@ -111,6 +72,98 @@ namespace MoscowNvcBot.Console
             const string Message = "Выберите PDF:";
 
             await Bot.SendTextMessageAsync(chatId, Message, replyMarkup: replyKeyboard);
+        }
+
+        private async Task SendAllGooglePdfAsync(long chatId, string fileName)
+        {
+            Task task = SendAllGooglePdfAsyncTask(chatId, fileName);
+
+            await WrapWithChatActionAsync(task, chatId, ChatAction.UploadDocument);
+        }
+
+        private async Task SendGooglePdfAsync(long chatId, DocumentInfo info)
+        {
+            Task task = SendGooglePdfAsyncTask(chatId, info);
+
+            await WrapWithChatActionAsync(task, chatId, ChatAction.UploadDocument);
+        }
+
+        private async Task SendAllGooglePdfAsyncTask(long chatId, string fileName)
+        {
+            string path = await Task.Run(() => UnifyInfos());
+
+            await SendFileAsync(chatId, fileName, path);
+
+            File.Delete(path);
+        }
+
+        private async Task WrapWithChatActionAsync(Task task, ChatId chatId, ChatAction chatAction)
+        {
+            using (var cancellatiomTokenSource = new CancellationTokenSource())
+            {
+                PeriodicSendChatActionAsync(chatId, chatAction, cancellatiomTokenSource.Token);
+
+                await task;
+
+                cancellatiomTokenSource.Cancel();
+            }
+        }
+
+        private async Task SendGooglePdfAsyncTask(long chatId, DocumentInfo info)
+        {
+            string fileName = await Task.Run(() => GetName(info));
+            string path = await Task.Run(() => CopyInfo(info));
+
+            await SendFileAsync(chatId, fileName, path);
+
+            File.Delete(path);
+        }
+
+        private string UnifyInfos()
+        {
+            IEnumerable<DocumentRequest> requests = _infos.Select(info => new DocumentRequest(info, 1));
+
+            string path = Path.GetTempFileName();
+            _googleDataManager.Unify(requests, path, false);
+
+            return path;
+        }
+
+        private async Task SendFileAsync(long chatId, string fileName, string path)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Open))
+            {
+                var pdf = new InputOnlineFile(fileStream, fileName);
+                await Bot.SendDocumentAsync(chatId, pdf);
+            }
+        }
+
+        private async void PeriodicSendChatActionAsync(ChatId chatId, ChatAction chatAction,
+            CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.WhenAny(Bot.SendChatActionAsync(chatId, chatAction, cancellationToken));
+                await Task.WhenAny(Task.Delay(ChatActionDuration, cancellationToken));
+            }
+        }
+
+        private string CopyInfo(DocumentInfo info)
+        {
+            var request = new DocumentRequest(info, 1);
+
+            string path = Path.GetTempFileName();
+            _googleDataManager.Copy(request, path, false);
+
+            return path;
+        }
+
+        private string GetName(DocumentInfo info)
+        {
+            string name = _googleDataManager.GetName(info.Id);
+            name = name.Replace("«", "");
+            name = name.Replace("»", "");
+            return $"{name}.pdf";
         }
     }
 }
