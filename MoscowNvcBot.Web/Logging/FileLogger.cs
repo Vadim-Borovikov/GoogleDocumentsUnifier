480362d64c1ff0df5820922ddca5427541c56962
Vadim Borovikov <vadim.borovikov@gmail.com>
2020-01-30 02:47:57 +0300

    Logger introduced

---
+++ MoscowNvcBot.Web/Logging/FileLogger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Logging;
+
+namespace MoscowNvcBot.Web.Logging
+{
+    internal class FileLogger : ILogger
+    {
+        private readonly string _path;
+        private readonly LogLevel _logLevel;
+
+        private static readonly object Lock = new object();
+
+        public FileLogger(string path, LogLevel logLevel)
+        {
+            _path = path;
+            _logLevel = logLevel;
+        }
+
+        public IDisposable BeginScope<TState>(TState state) => null;
+
+        public bool IsEnabled(LogLevel logLevel) => logLevel >= _logLevel;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception,
+            Func<TState, Exception, string> formatter)
+        {
+            if (IsEnabled(logLevel) && (formatter != null))
+            {
+                lock (Lock)
+                {
+                    File.AppendAllText(_path, formatter(state, exception) + Environment.NewLine);
+                }
+            }
+        }
+    }
+}
---
+++ MoscowNvcBot.Web/Logging/FileLoggerProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MoscowNvcBot.Web.Logging
+{
+    internal class FileLoggerProvider : ILoggerProvider
+    {
+        private readonly string _path;
+        private readonly LogLevel _logLevel;
+
+        public FileLoggerProvider(string path, LogLevel logLevel)
+        {
+            _path = path;
+            _logLevel = logLevel;
+        }
+
+        public ILogger CreateLogger(string categoryName) { return new FileLogger(_path, _logLevel); }
+
+        public void Dispose() { }
+    }
+}
