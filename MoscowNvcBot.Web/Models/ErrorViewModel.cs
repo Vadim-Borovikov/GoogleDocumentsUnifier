432961e89c3392ef0532229d243b7d19ebe58502
Vadim Borovikov <vadim.borovikov@gmail.com>
2020-10-11 23:05:32 +0300

    Bot removed

--- MoscowNvcBot.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MoscowNvcBot.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; internal set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
